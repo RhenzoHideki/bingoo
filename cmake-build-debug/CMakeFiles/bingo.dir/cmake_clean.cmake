dac87b860bd39cfed1f804a1fb8e6351eb609b36
Mike Domingues <mikeicd@gmail.com>
2020-02-13 09:59:42 -0300

    QQQ

---
+++ cmake-build-debug/CMakeFiles/bingo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bingo.dir/main.c.o"
+  "bingo"
+  "bingo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bingo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
